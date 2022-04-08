175f57002cd4941de627d029b9da1ebff72f3757
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-04-08 15:32:50 -0300

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,18 +8,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   return (
     <>
       <div>
-        <Rate onClick={() => setGood(good + 1)} text="Good" />
-        <Rate onClick={() => setNeutral(neutral + 1)} text="Neutral" />
-        <Rate onClick={() => setBad(bad + 1)} text="Bad" />
+        <Rate onClick={() => {setGood(good + 1); setTotal(total + 1)} } text="Good" />
+        <Rate onClick={() => {setNeutral(neutral + 1); setTotal(total + 1)}} text="Neutral" />
+        <Rate onClick={() => {setBad(bad + 1); setTotal(total + 1)}} text="Bad" />
       </div>
       <div>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>Total votes: {total}</p>
+        <p>Average: { (good - bad) / total }</p>
+        <p>Positive: { (good / total) * 100 }%</p>
       </div>
     </>
   );
