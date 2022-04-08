63361d335abe61b1c6648832dc0f02aa504cbaf7
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-04-08 15:49:34 -0300

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,14 @@ const Rate = (props) => {
   return <button onClick={props.onClick}>{props.text}</button>;
 };
 
+const Statistics = ({ text, rate }) => {
+  return (
+    <p>
+      {text}: {rate}
+    </p>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -13,18 +21,34 @@ const App = () => {
   return (
     <>
       <div>
-        <Rate onClick={() => {setGood(good + 1); setTotal(total + 1)} } text="Good" />
-        <Rate onClick={() => {setNeutral(neutral + 1); setTotal(total + 1)}} text="Neutral" />
-        <Rate onClick={() => {setBad(bad + 1); setTotal(total + 1)}} text="Bad" />
+        <Rate
+          onClick={() => {
+            setGood(good + 1);
+            setTotal(total + 1);
+          }}
+          text="Good"
+        />
+        <Rate
+          onClick={() => {
+            setNeutral(neutral + 1);
+            setTotal(total + 1);
+          }}
+          text="Neutral"
+        />
+        <Rate
+          onClick={() => {
+            setBad(bad + 1);
+            setTotal(total + 1);
+          }}
+          text="Bad"
+        />
       </div>
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total votes: {total}</p>
-        <p>Average: { (good - bad) / total }</p>
-        <p>Positive: { (good / total) * 100 }%</p>
-      </div>
+      <Statistics text="Good" rate={good} />
+      <Statistics text="Neutral" rate={neutral} />
+      <Statistics text="Bad" rate={bad} />
+      <Statistics text="Total" rate={total} />
+      <p>Average: {(good - bad) / total}</p>
+      <p>Positive: {(good / total) * 100}%</p>
     </>
   );
 };
