7186b4394e247a377da87bbcefa8611e12039139
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-04-25 16:42:34 -0300

    2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filters, setFilters] = useState("");
+  const [filterPerson, setFilterPerson] = useState([]);
 
   const onNameChange = (event) => {
     setNewName(event.target.value);
@@ -34,10 +41,21 @@ const App = () => {
     return persons.some((person) => person.name === name);
   };
 
+  const filter = (event) => {
+    setFilters(event.target.value);
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(filters)
+    );
+    setFilterPerson(filtered);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
+        <div>
+          filter name <input onChange={filter} />
+        </div>
         <div>
           name: <input onChange={onNameChange} value={newName} />
         </div>
@@ -51,12 +69,19 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>
-          <strong>Name:</strong> {person.name}, <strong>Number:</strong>{" "}
-          {person.number}
-        </p>
-      ))}
+      {filters === ""
+        ? persons.map((person) => (
+            <p key={person.id}>
+              <strong>Name:</strong> {person.name}, <strong>Number:</strong>{" "}
+              {person.number}
+            </p>
+          ))
+        : filterPerson.map((person) => (
+            <p key={person.id}>
+              <strong>Name:</strong> {person.name}, <strong>Number:</strong>{" "}
+              {person.number}
+            </p>
+          ))}
     </div>
   );
 };
